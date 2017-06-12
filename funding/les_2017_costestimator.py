8cb81155a9faa7013c16967d0cf943e0a09f6b33
Jonathon J Smith <Jonathon.J.Smith@jpl.nasa.gov>
2017-06-11 18:21:16 -0700

    More updates.

--- funding/les_2017_costestimator.py
+++ funding/les_2017_costestimator.py
@@ -1,4 +1,3 @@
-
 import os
 from os.path import join, exists
 from jinja2 import Environment, FileSystemLoader
@@ -13,8 +12,11 @@ TEX_TABLE = True
 # =====================================================================
 # Inputs
 
+WEBSITE_COST = 12000. # Total wag, not even sure want this
+
 # Define price variables
-price_booklet = 0.07 # USD per item
+price_test_booklet = 0.07 # USD per item
+price_cntrl_booklet = 0.03 # USD per item
 price_sticker = 0.15 # USD per item, derived from email titled "LES Article"
                     # which stated "[I] filled in the cost of the stickers
                     # for a 50K run (~$8K)", 8000 / 50000. = ~.15
@@ -22,18 +24,15 @@ price_labor = 15 # USD per hour
 price_incentive = 15 # USD per survey
 
 # Define survey variables
-R_RATE = 0.03 # survey response rate
+R_RATE = 0.05 # survey response rate
 NB_TEST = 75000
 NB_CNTRL = 75000
 NB_TOTAL  = NB_TEST + NB_CNTRL
 
 print("\n### Vegan Outreach Budget Summary for LES")
-print("   with partners Animal Charity Evaluations,")
-print("   The Humane League, and 80K Hours.")
 
 # Keep track of the total donatations of each partner org
 vo_total = 0
-thl_total = 0
 ace_total = 0
 cntrl_total = 0
 
@@ -52,52 +51,51 @@ labor_total = num_days * price_labor * 8.
 
 large_school_cost = BPD_large_ratio * labor_total
 small_school_cost = BPD_small_ratio * labor_total
-
+school_labor_cost = large_school_cost+small_school_cost
 
 print("\nLABOR")
 print("   With a Booklet Per Day (BPD) average of {0}".format(BPD_large))
 print("   for large schools and {0} for small schools,".format(BPD_small))
 print("   a total of {0} schools will need to be visited".format(num_schools))
 print("   to hand out the full {0} study complement of books.".format(NB_TOTAL))
-print("   ... Vegan Outreach Labor Donation: ${0}".format( small_school_cost ))
-print("   ... The Humane League Labor Donation: ${0}".format( large_school_cost))
+print("   ... Vegan Outreach Labor Donation: ${0}".format( labor_total ))
 
-vo_total += small_school_cost
-thl_total += large_school_cost
+vo_total += school_labor_cost
 
 sticker_rate = 100. # per hour
 sticker_labor_cost = ( NB_TOTAL / sticker_rate ) * price_labor
+print("\n   Attaching stickers to booklets at the rate of {0}/hr".format(sticker_rate))
+print("   will require a total of {0} hrs for the study.".format(NB_TOTAL / sticker_rate))
+print("   ... Vegan Outreach Labor Donation: ${0}".format(sticker_labor_cost))
+
+vo_total += sticker_labor_cost
 
 # For table
 labor_budget = [
     ("Attaching stickers", sticker_labor_cost, "Vegan Outreach"),
-    ("Leafleting (Small School)", small_school_cost, "Vegan Outreach"),
-    ("Leafleting (Large School)", large_school_cost, "Humane League"),
+    ("Leafleting", school_labor_cost, "Vegan Outreach"),
     ]
 
 # =====================================================================
 # =====================================================================
-# Booklet donations (VO, ACE, CTRL)
+# Booklet donations (VO, ACE)
 
-test_cost = NB_TEST * price_booklet
-cntrl_cost = NB_CNTRL * price_booklet
+test_cost = NB_TEST * price_test_booklet
+cntrl_cost = NB_CNTRL * price_cntrl_booklet
 sticker_cost = NB_TOTAL * price_sticker
 
 print("\nBOOKLETS AND STICKERS")
 print("   A total of {0} booklets are being handed".format(NB_TOTAL))
 print("   out ({0} test, {1} control). Stickers".format(NB_TEST,NB_CNTRL))
 print("   advertising the survey are attached.")
-print("   ... Vegan Outreach provides test booklets: ${0}".format(test_cost))
-print("   ... 80K Hours provides control booklets: ${0}".format(cntrl_cost))
+print("   ... Vegan Outreach provides booklets: ${0}".format(test_cost+cntrl_cost))
 print("   ... ACE Provides stickers: ${0}".format(sticker_cost))
 
-vo_total += test_cost
-cntrl_total += cntrl_cost
+vo_total += test_cost + cntrl_cost
 ace_total += sticker_cost
 
 material_budget = [
-    ("Test Booklets", test_cost, "Vegan Outreach"),
-    ("Control Booklets", cntrl_total, "Vegan Outreach"),
+    ("Booklets", test_cost + cntrl_cost, "Vegan Outreach"),
     ("Stickers", sticker_cost, "ACE"),
     ]
 
@@ -109,9 +107,9 @@ incentive_cost = NB_TOTAL * R_RATE * price_incentive
 
 print("\nSURVEY INCENTIVES")
 print("   A ${0} incentive is offered for students".format(price_incentive))
-print("   recieving a booklet to take the survey.")
+print("   recieving a booklet to take the surveys.")
 print("   An estimated {0} students will respond ({1} response rate)".format(R_RATE * NB_TOTAL, R_RATE))
-print("   ... ACE provides incentives: {0}".format(incentive_cost))
+print("   ... ACE provides incentives: ${0}".format(incentive_cost))
 
 ace_total += incentive_cost
 
@@ -130,9 +128,18 @@ print("   ... offer a ${0} incentive for a {1} survey response rate".format(pric
 
 print("\n### COST SHARING SUMMARY")
 print("   ...Vegan Outreach: ${0}".format(vo_total))
-print("   ...Humane League: ${0}".format(thl_total))
-print("   ...80K Hours: ${0}".format(cntrl_total))
 print("   ...ACE: ${0} <-- ACE Grant Request".format(ace_total))
+
+summary = {
+   'num_books' : NB_TOTAL,
+   'num_schools' : num_schools,
+   'num_cards' : NB_TOTAL * R_RATE,
+   'sticker_cost' : sticker_cost,
+   'website_cost' : WEBSITE_COST,
+   'card_cost' : incentive_cost,
+   'ace_total' : ace_total,
+   'vo_total'  : vo_total,
+   }
 
 # =====================================================================
 # =====================================================================
@@ -141,6 +148,7 @@ print("   ...ACE: ${0} <-- ACE Grant Request".format(ace_total))
 if TEX_TABLE:
     tex_table = env.get_template('budget_textable.tmpl')
     print tex_table.render(
+        summary=summary,
         labor_budget=labor_budget,
         material_budget=material_budget,
         incentive_budget=incentive_budget
